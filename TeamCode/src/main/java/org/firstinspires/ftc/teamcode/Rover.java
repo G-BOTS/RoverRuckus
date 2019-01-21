e5043588f45f2cb83d8345776ee0f9eee8e5c4d8
G-BOTS <ferasmus@wlcalv.com>
2019-01-21 15:13:19 -0800

    added rev hub

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rover.java
@@ -0,0 +1,75 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+
+/**
+ * Created by robot3050 on 10/26/2018.
+ */
+
+public class Rover
+{
+
+    public DcMotor leftMotor   = null;
+    public DcMotor rightMotor  = null;
+    public DcMotor Lift = null;
+    public DcMotor Hook = null;
+    public DcMotor Arm  = null;
+    public DcMotor Wrist = null;
+    public DcMotor Intake= null;
+    public DistanceSensor sensorRange = null;
+
+     //public DcMotor LiftMotor   = null;
+
+    HardwareMap ahwmap = null;
+
+
+    public void init(HardwareMap hwmap)
+    {
+        ahwmap = hwmap;
+
+        leftMotor = ahwmap.get(DcMotor.class, "left_drive");
+        rightMotor = ahwmap.get(DcMotor.class, "right_drive");
+        Lift = ahwmap.get(DcMotor.class, "lift");
+        Hook = ahwmap.get(DcMotor.class, "hook");
+        Arm = ahwmap.get(DcMotor.class,"arm");
+        Wrist = ahwmap.get(DcMotor.class,"wrist");
+        Intake  =  ahwmap.get(DcMotor.class,"intake");
+
+        //sensorRange = ahwmap.get(DistanceSensor.class,"sensor_range");
+
+        // LiftMotor = ahwmap.get(DcMotor.class,"LiftMotor");
+
+        leftMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+        Lift.setDirection(DcMotor.Direction.FORWARD);
+        Hook.setDirection(DcMotor.Direction.FORWARD);
+        Arm.setDirection(DcMotor.Direction.FORWARD);
+        Wrist.setDirection(DcMotor.Direction.FORWARD);
+        Intake.setDirection(DcMotor.Direction.FORWARD);
+
+
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+        Lift.setPower(0);
+        Hook.setPower(0);
+        Arm.setPower(0);
+        Wrist.setPower(0);
+        Intake.setPower(0);
+
+        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Hook.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Wrist.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+
+        //Rev2mDistanceSensor sensorTimeOfFlight = (Rev2mDistanceSensor)sensorRange;
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopRover.java
@@ -0,0 +1,122 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by robot3050 on 10/26/2018.
+ */
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="Rover: TeleopRover", group="Rover")
+public class TeleopRover extends OpMode {
+
+    Rover robot = new Rover();
+
+
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+        robot.Arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.Arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //int ArmTarget;
+    }
+
+    @Override
+        public void loop() {
+        robot.leftMotor.setPower(gamepad1.left_stick_y);
+        robot.rightMotor.setPower(gamepad1.right_stick_y);
+
+        if (gamepad1.right_bumper) {
+            robot.Lift.setPower(-1);
+        } else if (gamepad1.right_trigger > .01) {
+            robot.Lift.setPower(1);
+        } else {
+            robot.Lift.setPower(0);
+        }
+
+        if (gamepad1.left_bumper) {
+
+            robot.Hook.setPower(-1);
+        } else if (gamepad1.left_trigger > .01) {
+            robot.Hook.setPower(1);
+        } else {
+            robot.Hook.setPower(0);
+        }
+        if (gamepad1.x) {
+           // int indicator;
+            //indicator = indicator ++;
+
+            robot.Arm.setPower(-.2);
+        } else if (gamepad1.y) {
+            robot.Arm.setPower(0.2);
+        } else {
+            robot.Arm.setPower(0);
+           // int ArmTarget;
+          // ArmTarget = robot.Arm.getCurrentPosition();
+            //robot.Arm.setTargetPosition(ArmTarget);
+           // robot.Arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+        }
+        if (gamepad1.a) {
+
+            robot.Wrist.setPower(-0.2);
+        } else if (gamepad1.b ) {
+
+            robot.Wrist.setPower(0.2);
+        } else {
+            robot.Wrist.setPower(0);
+        }
+        if (gamepad1.dpad_up) {
+
+            robot.Intake.setPower(-0.2);
+        } else if (gamepad1.dpad_down) {
+
+            robot.Intake.setPower(0.2);
+        } else {
+            robot.Intake.setPower(0);
+        }
+/*
+        if (gamepad1.y) {
+            robot.Lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.Lift.setTargetPosition(-12000);
+        } else if (gamepad1.a) {
+            robot.Lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.Lift.setTargetPosition(-100);
+        }
+
+        while (robot.Lift.getCurrentPosition() < robot.Lift.getTargetPosition()) {
+            robot.Lift.setPower(1.0);
+        }
+
+        while (robot.Lift.getCurrentPosition() > robot.Lift.getTargetPosition()) {
+            robot.Lift.setPower(-1.0);
+        }
+
+        robot.Lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        if (gamepad1.x) {
+            robot.Hook.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.Hook.setTargetPosition(-1000);
+        } else if (gamepad1.b) {
+            robot.Hook.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.Hook.setTargetPosition(-10);
+        }
+
+
+        while (robot.Hook.getCurrentPosition() < robot.Hook.getTargetPosition()) {
+            robot.Hook.setPower(1.0);
+        }
+
+        while (robot.Hook.getCurrentPosition() > robot.Hook.getTargetPosition()) {
+            robot.Hook.setPower(-1.0);
+        }
+
+        robot.Hook.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+*/
+        telemetry.addData("Lift Encoder", robot.Lift.getCurrentPosition());
+        telemetry.addData("Hook Encoder", robot.Hook.getCurrentPosition());
+    }
+}
